e20b6a3cf36118b3c7cdd000720b3b6aea832b9e
dailywage <ankit.kumar2@delhivery.com>
2017-01-05 15:25:59 +0530

    Adding new route

--- src/main/java/org/delhivery/controllers/sampleController.java
+++ src/main/java/org/delhivery/controllers/sampleController.java
@@ -1,5 +1,6 @@
 package org.delhivery.controllers;
 
+import org.delhivery.resources.Employee;
 import org.delhivery.resources.greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class greetingController {
+public class sampleController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
@@ -18,5 +19,13 @@ public class greetingController {
     public greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new greeting(counter.incrementAndGet(),
                 String.format(template, name));
+    }
+
+    @RequestMapping("/getEmployee")
+    public Employee getEmployee(@RequestParam(value="id") int id,
+                                @RequestParam(value="name") String name,
+                                @RequestParam(value="role") String role){
+        return new Employee(id, name, role);
+
     }
 }
